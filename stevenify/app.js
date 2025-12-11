fe7ef776e6937275b6fa02f60e8d1b875dff9044
dcs0spb <s.p.bradley@durham.ac.uk>
2025-12-11 16:55:40 +0000

    Restructured for test, first test passes

---
+++ stevenify/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs');
+
+let songs = require("./songs.json")
+
+
+app.use(express.static('client'));
+app.use(express.json());
+
+
+app.get('/song/list', function (req, resp){
+    resp.send(songs);
+});
+
+app.post('/song/new', function(req, resp){
+  console.log("Post add new");
+  console.log("body is");
+  console.log(req.body);
+  songs.push(req.body.songtitle);
+  fs.writeFileSync('./songs.json', JSON.stringify(songs));
+  resp.send("Thank you for you interest in stevenify. Your request has not been put into a queue")
+})
+
+module.exports = app;
--- stevenify/server.js
+++ stevenify/server.js
@@ -1,26 +1,3 @@
-const express = require('express')
-const app = express()
+const app = require("./app");
 
-const fs = require('fs');
-
-let songs = require("./songs.json")
-
-
-app.use(express.static('client'));
-app.use(express.json());
-
-
-app.get('/song/list', function (req, resp){
-    resp.send(songs);
-});
-
-app.post('/song/new', function(req, resp){
-  console.log("Post add new");
-  console.log("body is");
-  console.log(req.body);
-  songs.push(req.body.songtitle);
-  fs.writeFileSync('./songs.json', JSON.stringify(songs));
-  resp.send("Thank you for you interest in stevenify. Your request has not been put into a queue")
-})
-
-app.listen(8090)
+app.listen(8090);
